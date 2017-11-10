6cc0a64220bdca7d196395dce2fd396faafbcb28
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-10 18:21:58 -0500

    Added api call to view list of people who bought a ticket to event

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
+
 class HomeController < ApplicationController
   def index
-  
+  	response = HTTParty.get('https://www.eventbriteapi.com/v3/events/39778577797/attendees/?token=WYYVY4N37HIWMKE57EY5')
+  	@people = response['attendees']
   end
 end
+
+		
