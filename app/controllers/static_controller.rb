9a868790f6c65c4027f2e811b3325b27bdb1b817
Raul <raulmartinez1855@gmail.com>
2017-11-13 17:25:57 -0500

    event model and view where we could enter the event id so that the system can be used for multiple events

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
-  def home
+  def event_home
+
+    @response = HTTParty.get("https://www.eventbriteapi.com/v3/events/39707355770/attendees/?token=R3MLTYFWNHNDB53GOBCP")
+
   end
 end
