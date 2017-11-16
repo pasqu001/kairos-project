221ca60f186c6844f6f157ba4a9d40c8d19e9973
KaseyPasqualini <kaseypasqualini@yahoo.com>
2017-11-16 14:54:13 -0500

    created home view for verify of photos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
-
+require 'kairos'
 class HomeController < ApplicationController
-  def index
-  	response = HTTParty.get('https://www.eventbriteapi.com/v3/events/39778577797/attendees/?token=WYYVY4N37HIWMKE57EY5')
-  	@people = response['attendees']
-  	num = 0
-  	@num = num
+
+  def verify_photo
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   post 'users/photos/new' => 'users#new_photo'
   post 'users/add_photo' => 'users#add_photo'
+  get '/verify_photo' => 'home#verify_photo'
+  post '/verify_photo' => 'home#verify_photo'
+
   resources :users
   resources :events
   root 'home#index'
