d760a05b25cff64335ed1d37d6d505b8093fc213
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-16 16:50:08 -0500

    added styling to the events page. Buttons, layout grid, background color.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
+	root 'events#index'
   post 'users/photos/new' => 'users#new_photo'
   post 'users/add_photo' => 'users#add_photo'
   resources :users
   resources :events
-  root 'home#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
