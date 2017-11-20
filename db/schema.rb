7bd726d9ed48fdba9f5c3db9b552644511333500
mau5atron <gabrielbetancourt96@outlook.com>
2017-11-20 12:53:18 -0500

    Final changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113215415) do
+ActiveRecord::Schema.define(version: 20171120062611) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.string "event_id"
